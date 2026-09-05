78c2e8fee8b337e98ee323f537bad450fd109ded
PieFlavr <94877667+PieFlavr@users.noreply.github.com>
2026-09-05 09:05:48 -0700

    Implemented basic Status Monitor

--- src/status_monitor.py
+++ src/status_monitor.py
@@ -6,13 +6,13 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class MinimalSubscriber(Node):
+class StatusMonitor(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('status_monitor')
         self.subscription = self.create_subscription(
             String,
-            'topic',
+            'lab0/status',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
@@ -24,14 +24,14 @@ class MinimalSubscriber(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    status_monitor = StatusMonitor()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(status_monitor)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    status_monitor.destroy_node()
     rclpy.shutdown()
 
 
